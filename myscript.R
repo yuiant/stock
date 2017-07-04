0baf26d28c9117ce9f4384f7e48b5337ec9e8c00
yuiant <465543035@qq.com>
2017-07-04 18:43:57 +0800

    7.4

--- myscript.R
+++ myscript.R
@@ -7,6 +7,8 @@ namer2p <- function(name){
 indexcount <- c(1,6,7,8,12,4,12,11,16,14,31,3,2,3)
 ind0 <- c(1,(cumsum(indexcount)+1)[-length(indexcount)])
 ind1 <- cumsum(indexcount)
+inds <- cbind(ind0,ind1)
+
 
 
 library(rjson)
@@ -15,22 +17,31 @@ namejson <- fromJSON(file = "id2token.json")
 
 ##initialize
 mc_ind <- read.csv("macro_indicator.csv",fileEncoding = "utf-8")
-col2rid <- read.csv("col2rid.csv",header = F)
+ridcol <- read.csv("col2rid.csv",header = F)
 
 xa <- rep(1:130,809)
 ya <- sort(rep(1:809,130))
 hm <- data.frame(xa,ya)
+hm$status <- as.integer(is.na(apply(hm,1,FUN = function(x){is.na(mc_ind[810-x[2],2+x[1]])})))
 txt <- paste(mc_ind$date[810-hm$ya],unlist(namejson)[hm$xa],sep = ":")
-
+hm$txt <- txt
 
 library(dplyr)
 library(tidyr)
 mc_ind$date <-as.character(mc_ind$date)
-submc <- filter(mc_ind,date >= "19992,01,1" & date <= "2016-11-01")
+submc <- filter(mc_ind,date >= "1992-01-01" & date <= "2016-11-01")
 
 
+p <- plot_ly(data = submc,x=~date) %>% 
+  add_trace(y = ~X02.00.01) %>%
+  add_trace(y=submc$X02.00.00)
 
+n <- dim(submc)[2]-2
+cm <- combn(n,2)
+a <- apply(cm,2,FUN = function(x){cor(submc[,x[1]+2],submc[,x[2]+2],use = "pairwise.complete.obs")})
+myname <- paste(names(submc)[3:132],unlist(namejson),sep = ":")
+cname <- apply(cm,2,FUN = function(x){paste(myname[x[1]],myname[x[2]],sep = "#")})
+cdf <- data.frame(cor = a, name = cname)
 
-
-
+write.table(cdf,"cdf.csv",sep = ",",fileEncoding = "utf-8")
 
